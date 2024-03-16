1086ecf5042d6c259dee273edb85384cbbc06abe
Lordphone Wen <lordphonewen@gmail.com>
2024-03-15 22:15:27 -0500

    migrated navigation bar and profile drop down. onto the contents

--- realworth/ClientApp/src/app/app.component.ts
+++ realworth/ClientApp/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [NavMenuComponent, RouterOutlet, RouterLink, RouterLinkActive]
+    imports: [NavigationBarComponent, RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'realworth';
+
+  showPPP = true;
+  showMap = false;
+
+  handleShowPPPChange(showPPP: boolean): void {
+    this.showPPP = showPPP;
+  }
+
+  handleShowMapChange(showMap: boolean): void {
+    this.showMap = showMap;
+  }
 }
 
--- realworth/ClientApp/src/app/app.server.module.ts
+++ realworth/ClientApp/src/app/app.server.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
 
 @NgModule({
     imports: [ServerModule],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    declarations: [
+      NavigationBarComponent,
+      ProfileDropdownComponent
+    ]
 })
 export class AppServerModule { }
 
