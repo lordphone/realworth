0d1ae3b5c25f3f76f07b402e36dd77b1a58cabe0
Lordphone Wen <lordphonewen@gmail.com>
2024-03-15 22:35:51 -0500

    migrated the purchasing power and world income map

--- realworth/ClientApp/src/app/app.component.ts
+++ realworth/ClientApp/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { WorldIncomeMapComponent } from './world-income-map/world-income-map.component';
+import { PurchasingPowerComponent } from './purchasing-power/purchasing-power.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [NavigationBarComponent, RouterOutlet, RouterLink, RouterLinkActive]
+    imports: [CommonModule, NgIf, NavigationBarComponent, RouterOutlet, RouterLink, RouterLinkActive, WorldIncomeMapComponent, PurchasingPowerComponent],
 })
 export class AppComponent {
   title = 'realworth';
