01b2e9de3b849f3f100387357b38f92bfd3ea20a
Lordphone Wen <lordphonewen@gmail.com>
2024-10-03 23:47:54 -0500

    finished working on the basic layout of the ppp page. next is migrating to aws and working on api/rds

--- realworth/ClientApp/src/app/world-income-map/world-income-map.component.ts
+++ realworth/ClientApp/src/app/world-income-map/world-income-map.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FetchDataComponent } from '../fetch-data/fetch-data.component';
 
 @Component({
   selector: 'app-world-income-map',
   standalone: true,
-  imports: [],
+  imports: [FetchDataComponent],
   templateUrl: './world-income-map.component.html',
   styleUrls: ['./world-income-map.component.css']
 })
